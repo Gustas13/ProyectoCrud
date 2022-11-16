c7431a53d41b7f3a00c39e43aa28c606e9b5ea12
Gustvo Gómez <luis.gomez6835@alumnos.udg.mx>
2022-11-16 16:12:09 -0600

    CRUD Funcionando

---
+++ app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empleado;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class EmpleadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $datos['empleado']=Empleado::paginate(5);
+        return view('empleado.index', $datos );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('empleado.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        //$datosEmpleado = request()->all();
+
+        $campos = [
+            'Nombre'=>'required|string|max:20',
+            'ApellidoPaterno'=>'required|string|max:100',
+            'ApellidoMaterno'=>'required|string|max:100',
+            'Correo'=>'required|mail',
+            'Foto'=>'required|max10000|mime:jpg,jpeg,png,gif',
+        ];
+        $mensaje = [
+                'required'=>'El :attribute es requerido',
+                'Foto.required' => 'La foto es requerida'
+
+        ];
+
+        $this->validate($request, $campos, $mensaje);
+
+        $datosEmpleado = request()->except('_token');
+
+        if($request->hasFile('Foto')){
+            $datosEmpleado['Foto']=$request->file('Foto')->store('uploads','public');
+        }
+        Empleado::insert($datosEmpleado);
+        //return response()->json($datosEmpleado);
+        return redirect('empleado')->with('mensaje','Empleado agregado con éxito');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Empleado $empleado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $empleado=Empleado::findOrFail($id);
+
+        return view('empleado.edit', compact('empleado') );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+       /* $campos=[
+            
+            'Nombre'=>'required|string|max:20',
+            'ApellidoPaterno'=>'required|string|max:100',
+            'ApellidoMaterno'=>'required|string|max:100',
+            'Correo'=>'required|mail',
+        ];
+
+        $mensaje = [
+            'required'=>'El :attribute es requerido',
+        ];
+
+        if($request->hasFile('Foto')){
+
+            $campos = ['Foto'=>'required|max10000|mime:jpg,jpeg,png,gif'];
+            $mensaje = ['Foto.required' => 'La foto es requerida'];
+        }
+
+        $this->validate($request, $campos,$mensaje);*/
+        //
+        $datosEmpleado = request()->except(['_token', '_method']);
+
+        if($request->hasFile('Foto')){
+            $empleado=Empleado::findOrFail($id);
+
+            Storage::delete('public/'.$empleado->Foto);
+
+            $datosEmpleado['Foto']=$request->file('Foto')->store('uploads','public');
+        }
+
+        Empleado::where('id', '=', $id) -> update($datosEmpleado);
+        $empleado=Empleado::findOrFail($id);
+        return view('empleado.edit', compact('empleado') );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Empleado  $empleado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $empleado=Empleado::findOrFail($id);
+
+        if(Storage::delete('public/'.$empleado->Foto)){
+
+            Empleado::destroy($id);
+        }
+
+        return redirect('empleado')->with('mensaje','Empleado borrado');
+    }
+}
